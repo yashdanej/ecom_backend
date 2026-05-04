b7754a0f58afbe840fab50767dd29d5df9eb7118
yashdanej <yashdanej2004@gmail.com>
2026-05-04 13:09:53 +0530

    changed cors

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "teridukan.vercel.app",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
